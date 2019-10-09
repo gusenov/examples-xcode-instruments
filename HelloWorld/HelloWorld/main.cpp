d6d7691d3ef270a8107576c7f7f83b5a17be294c
Аббас Гусенов <gusenov@live.ru>
2019-10-10 01:34:00 +0600

    Добавил пустой Xcode-проект "Hello, World!".

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Abbas Gussenov on 10/9/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
